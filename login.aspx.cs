4b693f9faa84eb5669532b5e699f2305a0388465
Tuncer Ergin <ergin.tuncer@hotmail.com>
2017-10-31 01:17:00 +0300

    PostgeSQL Bağlantısı Eklendi

--- login.aspx.cs
+++ login.aspx.cs
@@ -5,9 +5,15 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.OleDb;
+using Npgsql;
 
 public partial class login : System.Web.UI.Page
 {
+
+    NpgsqlConnection tCon = new NpgsqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["NpgsqlConnectionStrings"].ConnectionString);
+    NpgsqlCommand tCommand = new NpgsqlCommand();
+    NpgsqlDataReader tDataReader;
+    String tSQL;
     protected void Page_Load(object sender, EventArgs e){
     }
 
@@ -16,6 +22,22 @@ public partial class login : System.Web.UI.Page
                             HttpContext.Current.Server.MapPath("sosyal.mdb"));
 
     protected void giris_Click(object sender, EventArgs e){
+
+        tCon.Open();
+
+        tCon.Close();
+
+
+        tSQL = "SELECT * FROM version"
+        tCommand = New NpgsqlCommand(tSQL, tCon)
+        tCon.Open()
+        tDataReader = tCommand.ExecuteReader
+        While tDataReader.Read
+            tVersionOffline = tDataReader("offlineversion")
+            tVersionOfflineDate = tDataReader("offlineversiondate")
+            tVersionDB = tDataReader("dbversion")
+        End While
+        tCon.Close()
         //try{
         //    string veri = "select * from kayit where eposta='" + kullaniciadi.Text + "' and sifre='" + sifre.Text + "'";
         //    con.Open();
