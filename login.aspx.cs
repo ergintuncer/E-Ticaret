faca47fab26a88934c5db6143e1257877388f4af
Fatih türkmenoğlu <fatih.turkmenoglu@hotmail.com>
2017-10-31 16:09:42 +0300

    Sql login

--- login.aspx.cs
+++ login.aspx.cs
@@ -14,56 +14,164 @@ public partial class login : System.Web.UI.Page
     NpgsqlCommand tCommand = new NpgsqlCommand();
     NpgsqlDataReader tDataReader;
     String tSQL;
+
+
+    // İnsert - Update - Delete işlemleri için
+    public void PublicExecuteNonQuery()
+    {
+        NpgsqlCommand tCommand = new NpgsqlCommand(tSQL, tCon);
+
+        if (tCon.State == System.Data.ConnectionState.Open)
+        {
+            tCon.Close();
+        }
+
+        tCommand.Connection = tCon;
+        tCommand.CommandType = System.Data.CommandType.Text;
+        tCommand.CommandTimeout = 60000;
+        tCommand.CommandText = tSQL;
+        tCon.Open();
+        tCommand.ExecuteNonQuery();
+        tCon.Close();
+    }
+    // -----------------------------------------------------------------------------------------------------------
+
+
+    // Select sorugular için İteger
+    public int PublicExecuteScalarInteger()
+    {
+        NpgsqlCommand tCommand = new NpgsqlCommand(tSQL, tCon);
+        //int tInteger;
+
+        if (tCon.State == System.Data.ConnectionState.Open)
+        {
+            tCon.Close();
+        }
+
+        tCon.Open();
+        tCommand.CommandType = System.Data.CommandType.Text;
+        tCommand.CommandTimeout = 60000;
+        tCommand.CommandText = tSQL;
+
+
+        //if ( tCommand.ExecuteScalar() != DBNull.Value )
+        //{
+        // tInteger =(int)tCommand.ExecuteScalar();
+
+        //}
+
+        tCon.Close();
+        return Convert.ToInt32(tCommand.ExecuteScalar());
+
+    }
+    // -----------------------------------------------------------------------------------------------------------
+
+
+    // Select sorugular için Double
+    public double PublicExecuteScalarDouble()
+    {
+        NpgsqlCommand tCommand = new NpgsqlCommand(tSQL, tCon);
+        //int double;
+
+        if (tCon.State == System.Data.ConnectionState.Open)
+        {
+            tCon.Close();
+        }
+
+        tCon.Open();
+        tCommand.CommandType = System.Data.CommandType.Text;
+        tCommand.CommandTimeout = 60000;
+        tCommand.CommandText = tSQL;
+
+
+        //if ( tCommand.ExecuteScalar() != DBNull.Value )
+        //{
+        // tInteger =(int)tCommand.ExecuteScalar();
+
+        //}
+
+        tCon.Close();
+        return Convert.ToDouble(tCommand.ExecuteScalar());
+
+    }
+    // -----------------------------------------------------------------------------------------------------------
+
+    // Select sorugular için String
+    public string PublicExecuteScalarString()
+    {
+        NpgsqlCommand tCommand = new NpgsqlCommand(tSQL, tCon);
+        //int string;
+
+        if (tCon.State == System.Data.ConnectionState.Open)
+        {
+            tCon.Close();
+        }
+
+        tCon.Open();
+        tCommand.CommandType = System.Data.CommandType.Text;
+        tCommand.CommandTimeout = 60000;
+        tCommand.CommandText = tSQL;
+
+
+        //if ( tCommand.ExecuteScalar() != DBNull.Value )
+        //{
+        // tInteger =(int)tCommand.ExecuteScalar();
+
+        //}
+
+        tCon.Close();
+        return Convert.ToString(tCommand.ExecuteScalar());
+
+    }
+    // -----------------------------------------------------------------------------------------------------------
+
+    // Select sorugular için Boolean
+        public Boolean PublicExecuteScalarBoolean()
+    {
+        NpgsqlCommand tCommand = new NpgsqlCommand(tSQL, tCon);
+        //int Boolean;
+
+        if (tCon.State == System.Data.ConnectionState.Open)
+        {
+            tCon.Close();
+        }
+
+        tCon.Open();
+        tCommand.CommandType = System.Data.CommandType.Text;
+        tCommand.CommandTimeout = 60000;
+        tCommand.CommandText = tSQL;
+
+
+        //if ( tCommand.ExecuteScalar() != DBNull.Value )
+        //{
+        // tInteger =(int)tCommand.ExecuteScalar();
+
+        //}
+
+        tCon.Close();
+        return Convert.ToBoolean(tCommand.ExecuteScalar());
+
+    }
+    // -----------------------------------------------------------------------------------------------------------
+
     protected void Page_Load(object sender, EventArgs e){
     }
 
-    OleDbConnection con =
-        new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; DATA Source=" +
-                            HttpContext.Current.Server.MapPath("sosyal.mdb"));
+    //OleDbConnection con =
+    //    new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; DATA Source=" +
+    //                        HttpContext.Current.Server.MapPath("sosyal.mdb"));
 
     protected void giris_Click(object sender, EventArgs e){
 
-        tCon.Open();
-        
-
-        tCon.Close();
+        tSQL = "INSERT INTO kisi_bilgi(ad,soyad) VALUES ('" + kullaniciadi.Text + "','" + sifre.Text + "')";
+        PublicExecuteNonQuery();
 
 
-        //tSQL = "SELECT * FROM version"
-        //tCommand = New NpgsqlCommand(tSQL, tCon)
-        //tCon.Open()
-        //tDataReader = tCommand.ExecuteReader
-        //While tDataReader.Read
-        //    tVersionOffline = tDataReader("offlineversion")
-        //    tVersionOfflineDate = tDataReader("offlineversiondate")
-        //    tVersionDB = tDataReader("dbversion")
-        //End While
-        //tCon.Close()
-        //try{
-        //    string veri = "select * from kayit where eposta='" + kullaniciadi.Text + "' and sifre='" + sifre.Text + "'";
-        //    con.Open();
-        //    OleDbCommand cmd = new OleDbCommand(veri, con);
-        //    OleDbDataReader oku = cmd.ExecuteReader();
-        //    if (oku.Read()){
-        //        if (kullaniciadi.Text == "123"){
-        //            Session.Add("kullanici", kullaniciadi.Text);
-        //            Response.Redirect("admin.aspx");
-        //        }
-        //        else{
 
-        //        }
-        Session.Add("kullanici", 1);
-        Response.Redirect("admin.aspx");
-        //    }
-        //    else{
-        //        lbl1.Visible = true;
-        //        lbl1.Text = "şifre veya Kullanici adı hatalı!!!";
-        //    }
-        //}
-        //catch{
-        //    lbl1.Visible = true;
-        //    lbl1.Text = "Bir Hata oluştu.Lütfen tekrar deneyiniz.";
-        //}
+       
+        //Session.Add("kullanici", 1);
+        //Response.Redirect("admin.aspx");
+      
     }
 
     protected void kayitol_Click(object sender, EventArgs e){
